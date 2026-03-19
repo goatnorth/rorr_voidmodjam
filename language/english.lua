bc685443059a2da881b0cd10ad9ac8e9c63abde9
gote <114143626+goatnorth@users.noreply.github.com>
2026-03-19 23:48:55 +0100

    THE LOG

--- language/english.lua
+++ language/english.lua
@@ -3,7 +3,7 @@ return {
 		Revenant = {
 			name = "Revenant",
 			nameUpper = "REVENANT",
-			description = "The <y>Revenant</c> is a walking tank who has finally found freedom, smashing anyone who gets in his way. Being a melee character he struggles with range at times, which is where his secondary comes in, it has a long range and it pulls enemies in. If you're surrounded use your utilitly to shred anyone close, and if there is somehow some fool still there, remove them from existence with the Corrupted Cannon",
+			description = "The <y>Revenant</c> is a walking tank who has finally found freedom, smashing anyone who gets in his way. Use your <y>Broken Arrows</c> to pull enemies in range of your <y>Smashing Blade</c>, shred groups and flyers with <y>Lashing Whip</c>, and blast those foolish enough to remain with a <y>Corrupted Cannon</c>.",
 			endQuote = "..and so he left, to be exiled once more.",
 			story = "An ancient warrior of the High Court, believing he set out on a pilgrimage, when in reality was exiled for his ideals. Never knowing this, he continued his journey and eventually encountered some sort of portal, being interested he entered. He regrets that decision to this very day. For that portal sent him to the void never to return, as most would think. He battled for his life for over 500 years being corrupted by the Void's essence and was nigh unrecongizable, in fact, he techincally wasn't a living being anymore. Just a spirit in a corrupted suit of armor. However, eventually he did break free and he felt euphoria for the first time in 500 years. Only to return home and be declared a heretic and a monster and exiled once more. He settled down at a lawless world and made a farm. They still say he still works there to this day.",
 			id = "Travel ID: 7304D719H810543 OOD",
@@ -13,7 +13,7 @@ return {
 	skill = {
 		RevenantZ = {
 			name = "Smashing Blade",
-			description = "Swing your blade in a combo <y>120% damage, with each part of the combo doing slightly more damage.</c>"
+			description = "Swing your blade in a combo for <y>120% damage, with each part of the combo doing slightly more damage.</c>"
 		},
 		RevenantX = {
 			name = "Broken Arrow",
